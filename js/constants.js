115cf1d4c542c2ea6d87d95ec26474ded8ec4b3c
Sasha Amirov <amirovalex98@gmail.com>
2021-11-01 20:18:24 +0200

    M1-(F)-Created the searchbar with results

---
+++ js/constants.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("searchButton");
+const input = document.getElementsByTagName("input")[0];
+const resultsList = document.getElementsByClassName("results-list")[0];
+const spinner = document.getElementById("loading");
+
+export default { button, input, resultsList, spinner };
---
+++ js/index.js
@@ -0,0 +1,51 @@
+const button = document.getElementById("searchButton");
+const input = document.getElementsByTagName("input")[0];
+const resultsList = document.getElementsByClassName("results-list")[0];
+const spinner = document.getElementById("loading");
+
+const clearResults = (childrenNum) => {
+  for (let i = 0; i < childrenNum - 1; i++) {
+    resultsList.removeChild(resultsList.lastChild);
+  }
+};
+
+const classChangesOnSearchStart = () => {
+  if (resultsList.children.length > 1) {
+    clearResults(resultsList.children.length);
+  }
+  resultsList.classList.remove("p-top-0");
+  spinner.classList.remove("d-none");
+  resultsList.classList.add("align-center");
+};
+
+const classChangesOnSearchFinish = () => {
+  spinner.classList.add("d-none");
+  resultsList.classList.remove("align-center");
+  resultsList.classList.add("p-top-0");
+};
+
+const onSearchCallAsync = async (query) => {
+  try {
+    classChangesOnSearchStart();
+    const response = await fetch(
+      `https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/search?query=${query}&limit=10&exchange=NASDAQ`
+    );
+    const data = await response.json();
+    data.map((stock) => {
+      const listItem = document.createElement("a");
+      listItem.href = `./company.html?symbol=${stock.symbol}`;
+      listItem.classList.add("result-tile");
+      listItem.innerText = `${stock.name} (${stock.symbol})`;
+      resultsList.appendChild(listItem);
+    });
+  } catch (err) {
+    throw new Error(err);
+  } finally {
+    classChangesOnSearchFinish();
+  }
+};
+
+button.addEventListener("click", async (e) => {
+  const response = await onSearchCallAsync(input.value);
+  return response;
+});
