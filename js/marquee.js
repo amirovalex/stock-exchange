8bacaa08bd279c2ffac44d8dc2d15da6794ff400
Sasha Amirov <amirovalex98@gmail.com>
2021-11-08 21:44:16 +0200

    M5-(F)-converted marquee to class

--- js/marquee.js
+++ js/marquee.js
@@ -1,59 +1,81 @@
-const fetchedActiveStocks = [];
-const fillArray = (arr, value) => {
-  arr.push(value);
-};
-
-function populateMarquee(selector, stockArray) {
-  const marquee = document.querySelector(`.${selector}`);
-  let stocks = `<h1 class="stock-name">`;
-  for (let i = 0; i < stockArray.length; ++i) {
-    stocks += `<span class="mrl-1">${stockArray[i].ticker} ${
-      stockArray[i].changes < 0 ? `<span class="red">` : `<span class="green">`
-    }${stockArray[i].changes.toString().slice(0, 4)}%</span> </span>`;
+class Marquee {
+  constructor(marqueeDom) {
+    this.marqueeDiv = marqueeDom;
+    this.i = 0;
+    this.fetchedActiveStocks = [];
   }
-  stocks += `</h1>`;
-  marquee.innerHTML = stocks;
+
+  fillArray = (arr, value) => {
+    arr.push(value);
+  };
+
+  fetchActiveStocks = async () => {
+    try {
+      if (this.fetchedActiveStocks.length === 0) {
+        const response = await fetch(
+          `https://financialmodelingprep.com/api/v3/actives?apikey=${apiKey}`
+        );
+        const data = await response.json();
+        data.slice(0, 10).map((stock, index) => {
+          this.fillArray(this.fetchedActiveStocks, stock);
+        });
+      } else {
+        this.fetchedActiveStocks.map((stock, index) => {
+          this.fillArray(this.fetchedActiveStocks, stock);
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  populateMarquee() {
+    let stocks = `<h1 class="stock-name">`;
+    for (let i = 0; i < this.fetchedActiveStocks.length; ++i) {
+      stocks += `<span class="mrl-1">${this.fetchedActiveStocks[i].ticker} ${
+        this.fetchedActiveStocks[i].changes < 0
+          ? `<span class="red">`
+          : `<span class="green">`
+      }${this.fetchedActiveStocks[i].changes
+        .toString()
+        .slice(0, 4)}%</span> </span>`;
+    }
+    stocks += `</h1>`;
+    this.marqueeDiv.innerHTML = stocks;
+  }
+
+  onCloneMarqueeH1() {
+    this.clonedMarquee = this.marqueeDiv.innerHTML;
+    this.firstMarqueeDivElement = this.marqueeDiv.children[0];
+    this.marqueeDiv.insertAdjacentHTML("beforeend", this.clonedMarquee);
+    this.marqueeDiv.insertAdjacentHTML("beforeend", this.clonedMarquee);
+  }
+
+  moveMarquee() {
+    setInterval(
+      function () {
+        this.firstMarqueeDivElement.style.marginLeft = `-${this.i}px`;
+        if (this.i > this.firstMarqueeDivElement.clientWidth) {
+          this.i = 0;
+        }
+        this.i = this.i + 0.2;
+      }.bind(this),
+      0
+    );
+  }
+
+  async runMarquee() {
+    try {
+      await this.fetchActiveStocks();
+      this.populateMarquee();
+      this.onCloneMarqueeH1();
+      this.moveMarquee();
+    } catch (err) {
+      console.log(err);
+    }
+  }
 }
 
-function Marquee(selector, speed, stock) {
-  const parentSelector = document.querySelector(selector);
-  const clone = parentSelector.innerHTML;
-  const firstElement = parentSelector.children[0];
-  let i = 0;
-  parentSelector.insertAdjacentHTML("beforeend", clone);
-  parentSelector.insertAdjacentHTML("beforeend", clone);
-
-  setInterval(function () {
-    firstElement.style.marginLeft = `-${i}px`;
-    if (i > firstElement.clientWidth) {
-      i = 0;
-    }
-    i = i + speed;
-  }, 0);
-}
-
-const fetchActiveStocks = async () => {
-  try {
-    if (fetchedActiveStocks.length === 0) {
-      const response = await fetch(
-        `https://financialmodelingprep.com/api/v3/actives?apikey=${apiKey}`
-      );
-      const data = await response.json();
-      data.slice(0, 10).map((stock, index) => {
-        fillArray(fetchedActiveStocks, stock);
-      });
-      populateMarquee("marquee", fetchedActiveStocks);
-      Marquee(".marquee", 0.2);
-    } else {
-      fetchedActiveStocks.map((stock, index) => {
-        fillArray(fetchedActiveStocks, stock);
-      });
-      populateMarquee("marquee", fetchedActiveStocks);
-      Marquee(".marquee", 0.2, fetchedActiveStocks);
-    }
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-fetchActiveStocks();
+const marqueeDom = document.querySelector(".marquee");
+const newMarquee = new Marquee(marqueeDom);
+newMarquee.runMarquee();
--- js/utils.js
+++ js/utils.js
@@ -2,7 +2,6 @@ const toggleSizeOnFetch = (onIndex = false) => {
   if (onIndex) {
     resultsList.classList.toggle("height-100");
   }
-  console.log("1");
   //   debugger;
   html.classList.toggle("height-100");
   body.classList.toggle("height-100");
